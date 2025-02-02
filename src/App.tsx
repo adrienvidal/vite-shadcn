b6ede699302217d79d194600cb66be4cfbf499bc
Adrien Vidal <adriensg.vidal@gmail.com>
2025-02-02 19:25:31 +0100

    clean

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,8 @@ import { Button } from '@/components/ui/button'
 function App() {
   return (
     <main>
-      <Button>Click me</Button>
+      <h1 class="text-red-600">Test tailwind</h1>
+      <Button>test shadcn</Button>
     </main>
   )
 }
