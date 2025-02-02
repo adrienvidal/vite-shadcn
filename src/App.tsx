8d456b53a5bd886dc38c8c6e955a57de84c1a0c1
Adrien Vidal <adriensg.vidal@gmail.com>
2025-02-02 19:14:27 +0100

    clean

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
-import { Button } from "@/components/ui/button"
+import { Button } from '@/components/ui/button'
 
 function App() {
-  return <main>
-    <Button>Click me</Button>
-  </main>
+  return (
+    <main>
+      <Button>Click me</Button>
+    </main>
+  )
 }
 
 export default App
